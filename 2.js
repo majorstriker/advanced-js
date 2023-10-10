fea4c329a970e0c87e554acdc55ed901fb0b73ef
majorstriker <majoryman90@gmail.com>
2023-10-10 21:08:38 +0300

    stage2

--- 2.js
+++ 2.js
@@ -1,30 +1,43 @@
-const cooks = new Map();
-cooks.set("Пицца", "Виктор");
-cooks.set("Суши", "Ольга");
-cooks.set("Десерты", "Дмитрий");
+const initialData = [{
+        product: "Apple iPhone 13",
+        reviews: [{
+                id: "1",
+                text: "Отличный телефон! Батарея держится долго.",
+            },
+            {
+                id: "2",
+                text: "Камера супер, фото выглядят просто потрясающе.",
+            },
+        ],
+    },
+    {
+        product: "Samsung Galaxy Z Fold 3",
+        reviews: [{
+            id: "3",
+            text: "Интересный дизайн, но дорогой.",
+        }, ],
+    },
+    {
+        product: "Sony PlayStation 5",
+        reviews: [{
+            id: "4",
+            text: "Люблю играть на PS5, графика на высоте.",
+        }, ],
+    },
+];
 
-const dishes = new Map();
-dishes.set("Маргарита", "Виктор");
-dishes.set("Пепперони", "Виктор");
-dishes.set("Филадельфия", "Ольга");
-dishes.set("Калифорния", "Ольга");
-dishes.set("Тирамису", "Дмитрий");
-dishes.set("Чизкейк", "Дмитрий");
-
-const alexOrders = new Set().add('Пепперони')
-    .add('Тирамису');
-const mariyaOrders = new Set().add('Калифорния')
-    .add('Маргарита');
-const irinaOrders = new Set().add('Чизкейк');
-
-const clients = new Map();
-clients.set("Алексей", alexOrders);
-clients.set("Мария", mariyaOrders);
-clients.set("Ирина", irinaOrders);
-
-console.log(`Пиццу готовит: ${cooks.get("Пицца")}`);
-console.log(`Заказ Алексея: ${[...clients.get("Алексей")]}`);
-console.log(`Суши готовит: ${cooks.get("Суши")}`);
-console.log(`Заказ Марии: ${[...clients.get("Мария")]}`);
-console.log(`Десерты готовит: ${cooks.get("Десерты")}`);
-console.log(`Заказ Ирины: ${[...clients.get("Ирина")]}`);
+checkInput.addEventListener('click', () => {
+    try {
+        const userInp = userInput.value;
+        if (userInp.length > 50 && userInp.length < 500 || !isNaN(userInp) || userInp.trim() === '') {
+            hintForUser.textContent = 'Отзыв должен быть не менее 50 символов, не более 500, состоять из текста, а не цифр и не допускаются пустые значения!';
+            throw new Error('Введите текст!');
+        } else {
+            initialData.push(userInp);
+            const html = initialData.map((item) => `<li>${item}</li>`).join('');
+            document.querySelector('.ratingData').innerHTML = html;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
